15387927fd99ed3001522c9f088e8a630217948e
Rekik Ziku <72988748+rekikziku@users.noreply.github.com>
2020-12-05 15:36:55 -0500

    Create struct.h

---
+++ struct.h
@@ -0,0 +1,15 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+#include <stdlib.h>
+
+struct intvalues{
+        int goal;
+        int c;
+        int total_productive;
+        int p, n;
+};
+
+int productivity calculate_productivity();
+
+#endif STRUCT_H
